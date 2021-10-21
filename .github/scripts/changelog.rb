f1e5ce7beb8f932f8dd96f5a05ecbf1ab84b88c5
Alexander Graul <agraul@suse.com>
2021-10-21 17:25:37 +0200

    Simplify changelog.rb
    Only .changes files are passed to this script by the get-changed-files
    action.

--- .github/scripts/changelog.rb
+++ .github/scripts/changelog.rb
@@ -1,16 +1,16 @@
 #! /usr/bin/ruby
 require 'json'
 
-# if true we exit with 0, so the test is sucessefull. otherwise the test fail.
-def check_changelog?(pr_body, files)
-  return true if pr_body.match(/\[x\]\s+No\s+changelog\s+needed/i)
-  
-  puts 'Looking for a .changes file...'
-  files.any? { |f| f.include? '.changes' } 
+def not_needed?(pr_body)
+  pr_body.match(/\[x\]\s+No\s+changelog\s+needed/i)
 end
 
 github_event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-files = ARGV[0].split(' ')
-puts "-" * 20, "PR Description: #{github_event['pull_request']['body']}"
-puts "-" * 20, "Files modified: #{files}"
-check_changelog?(github_event['pull_request']['body'], files) ?  exit(0) : exit(1)
+pr_body = github_event['pull_request']['body']
+changes_files = ARGV
+puts '-' * 20, "PR Description: #{github_event['pull_request']['body']}"
+puts '-' * 20, "Added or modified .changes files: #{changes_files}"
+
+exit(0) unless changes_files.empty?
+
+not_needed?(pr_body) ? exit(0) : exit(1)
